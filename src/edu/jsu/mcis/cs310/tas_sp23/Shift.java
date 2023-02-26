03dcf383315c37fbcf0138a3b8f683072f415f98
Brandon <bpugh@stu.jsu.edu>
2023-02-25 20:47:46 -0600

    Added Shift class

---
+++ src/edu/jsu/mcis/cs310/tas_sp23/Shift.java
@@ -0,0 +1,43 @@
+package edu.jsu.mcis.cs310.tas_sp23;
+
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Brandon Pugh
+ */
+
+
+public class Shift {
+    private int lunchDuration; // minutes
+    private int shiftDuration; // minutes
+    
+    
+    public Shift(HashMap<String, String> shiftParams) {
+        String lunchStr = shiftParams.get("lunchduration");
+        this.lunchDuration = Integer.parseInt(lunchStr);
+        
+        String shiftStr = shiftParams.get("shiftduration");
+        this.shiftDuration = Integer.parseInt(shiftStr);
+    }
+    
+    public int getLunchDuration() {
+        return this.lunchDuration;
+    }
+    
+    public int getShiftDuration() {
+        return this.shiftDuration;
+    }
+    
+    public void setLunchDuration(int lunchDuration) {
+        this.lunchDuration = lunchDuration;
+    }
+    
+    public void setShiftDuration(int shiftDuration) {
+        this.shiftDuration = shiftDuration;
+    }
+    // Basics of the shift class so far
+    // Also wanted to make sure I could push it to Git
+    
+}
