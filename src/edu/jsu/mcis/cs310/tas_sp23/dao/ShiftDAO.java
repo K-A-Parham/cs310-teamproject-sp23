6ee37f4bd8f05bec2633edac383f42a30d5d1a76
Makiya Kirby <mkirby8@stu.jsu.edu>
2023-02-27 12:47:29 -0600

    Added a Query for a find() method in ShiftDAO.java

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/ShiftDAO.java
@@ -1,10 +1,19 @@
 package edu.jsu.mcis.cs310.tas_sp23.dao;
 
+import edu.jsu.mcis.cs310.tas_sp23.Shift;
+import java.sql.*;
 
-/**
- *
- * @author Brandon Pugh
- */
+
 public class ShiftDAO {
+    
+    private static final String QUERY_FIND = "SELECT * FROM badge WHERE id = ?";
+
+    private final DAOFactory daoFactory;
+    
+    ShiftDAO(DAOFactory daoFactory) {
+
+        this.daoFactory = daoFactory;
+
+    }
     
 }
