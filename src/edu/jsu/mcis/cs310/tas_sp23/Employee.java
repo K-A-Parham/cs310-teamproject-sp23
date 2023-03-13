bd5bc6a364a56ee9f01a5c50f6b93a66e2436fcc
Brandon <bpugh@stu.jsu.edu>
2023-03-13 12:23:57 -0500

    Employee Class Finished

---
+++ src/edu/jsu/mcis/cs310/tas_sp23/Employee.java
@@ -0,0 +1,85 @@
+
+package edu.jsu.mcis.cs310.tas_sp23;
+import java.time.*;
+import java.util.*;
+import java.time.temporal.ChronoUnit;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Brandon Pugh
+ */
+public class Employee { 
+    private int id;
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private LocalDateTime active;
+    private Badge badge;
+    private String Department;
+    private String Shift;
+    private String EmployeeType;
+    
+    public Employee(int id, String firstname, String middlename, String lastname, 
+            LocalDateTime active, Badge badge, String Department, String Shift, String EmployeeType)
+    {
+        this.id = id;
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.active = active;
+        this.badge = badge;
+        this.Department = Department;
+        this.Shift = Shift;
+        this.EmployeeType = EmployeeType;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    public String getFirstname(){
+        return firstname;
+    }
+    public String getMiddlename(){
+        return middlename;
+    }
+    public String getLastname(){
+        return lastname;
+    }
+    public LocalDateTime getActive(){
+        return active;
+    }
+    public Badge getBadge(){
+        return badge;
+    }
+    public String getDepartment(){
+        return Department;
+    }
+    public String getShift(){
+        return Shift;
+    }
+    public String getEmployeeType(){
+        return EmployeeType;
+    }
+    
+    @Override
+    public String toString(){
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String date = active.format(formatter);
+        
+         StringBuilder stringbuilder = new StringBuilder();
+         
+         stringbuilder.append('#').append(id).append(' ');
+         stringbuilder.append('(').append(firstname).append(')');
+         stringbuilder.append('(').append(middlename).append(')');
+         stringbuilder.append('(').append(lastname).append(')');
+         stringbuilder.append(" (").append(badge).append(") ");
+         stringbuilder.append("Type: ").append(Shift).append(" ");
+         stringbuilder.append("Department: ").append(Department).append(" ");
+         stringbuilder.append("Active: ").append(active).append(" ");
+         
+         return stringbuilder.toString();
+    }
+}
+
