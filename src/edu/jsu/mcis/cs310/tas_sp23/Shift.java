ffc10b10f1def27dae45e515d5e3d77b1b3abf7f
Makiya Kirby <mkirby8@stu.jsu.edu>
2023-03-13 13:43:32 -0500

    Made changes/corrections to Shift.java and ShiftDAO.java.

--- src/edu/jsu/mcis/cs310/tas_sp23/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Shift.java
@@ -1,6 +1,9 @@
 package edu.jsu.mcis.cs310.tas_sp23;
 
 
+//import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 
 
@@ -8,55 +11,46 @@ import java.util.HashMap;
 
 public class Shift {
     
-    private final String id, description, shiftstart, shiftstop, roundinterval;
-    private final String graceperiod, dockpenalty, lunchstart, lunchstop, lunchthreshold;
+    private final String description;
+    private final int id, roundinterval, graceperiod, dockpenalty, lunchthreshold;
+    private final LocalTime shiftstart, shiftstop, lunchstart, lunchstop;
+    
     private int lunchDuration; // minutes
     private int shiftDuration; // minutes
-    private String name;
-    //private int id;
-    
-    
     
     public Shift(HashMap<String, String> shiftParams) {
-        String idStr = shiftParams.get("id");
-        this.id = idStr;
         
-        String descriptionStr = shiftParams.get("description");
-        this.description = descriptionStr;
+        this.id = Integer.parseInt( shiftParams.get("id") );
+        this.roundinterval = Integer.parseInt( shiftParams.get("roundinterval") );
+        this.graceperiod = Integer.parseInt( shiftParams.get("graceperiod") );
+        this.dockpenalty = Integer.parseInt( shiftParams.get("dockpenalty") );
+        this.lunchthreshold = Integer.parseInt( shiftParams.get("lunchthreshold") );
+        
+        this.description = shiftParams.get("description");
         
         String shiftstartStr = shiftParams.get("shiftstart");
-        this.shiftstart = shiftstartStr;
-        
+        LocalTime parsedTime = LocalTime.parse(shiftstartStr); //this.shiftstart  LocalTime parsedDate
+        this.shiftstart = parsedTime;
+                
         String shiftstopStr = shiftParams.get("shiftstop");
-        this.shiftstop = shiftstopStr;
-        
-        String roundintervalStr = shiftParams.get("roundinterval");
-        this.roundinterval = roundintervalStr;
-        
-        String graceperiodStr = shiftParams.get("graceperiod");
-        this.graceperiod = graceperiodStr;
-        
-        String dockpenaltyStr = shiftParams.get("dockpenalty");
-        this.dockpenalty = dockpenaltyStr;
+        LocalTime parsedTime_two = LocalTime.parse(shiftstopStr);
+        this.shiftstop = parsedTime_two;
         
         String lunchstartStr = shiftParams.get("lunchstart");
-        this.lunchstart = lunchstartStr;
+        LocalTime parsedTime_three = LocalTime.parse(lunchstartStr);
+        this.lunchstart = parsedTime_three;
+        //this.lunchstart = lunchstartStr;
         
         String lunchstopStr = shiftParams.get("lunchstop");
-        this.lunchstop = lunchstopStr;
+        LocalTime parsedTime_four = LocalTime.parse(lunchstopStr);
+        this.lunchstop = parsedTime_four;
         
-        String lunchthresholdStr = shiftParams.get("lunchthreshold");
-        this.lunchthreshold = lunchthresholdStr;
+        this.shiftDuration = (int)ChronoUnit.MINUTES.between(shiftstart, shiftstop);
+        this.lunchDuration = (int)ChronoUnit.MINUTES.between(lunchstart, lunchstop);
         
-        
-        String lunchStr = shiftParams.get("lunchduration");
-        this.lunchDuration = Integer.parseInt(lunchStr);
-        
-        String shiftStr = shiftParams.get("shiftduration");
-        this.shiftDuration = Integer.parseInt(shiftStr);
     }
     
-    public String getId() {
+    public int getId() {
         return id;
     }
     
@@ -64,35 +58,35 @@ public class Shift {
         return description;
     }
     
-    public String getShiftStart() {
+    public LocalTime getShiftStart() {
         return shiftstart;
     }
     
-    public String getShiftStop() {
+    public LocalTime getShiftStop() {
         return shiftstop;
     }
     
-    public String getRoundInterval() {
+    public int getRoundInterval() {
         return roundinterval;
     }
     
-    public String getGracePeriod() {
+    public int getGracePeriod() {
         return graceperiod;
     }
     
-    public String getDockPenalty() {
+    public int getDockPenalty() {
         return dockpenalty;
     }
     
-    public String getLunchStart() {
+    public LocalTime getLunchStart() {
         return lunchstart;
     }
     
-    public String getLunchStop() {
+    public LocalTime getLunchStop() {
         return lunchstop;
     }
     
-    public String getLunchThreshold() {
+    public int getLunchThreshold() {
         return lunchthreshold;
     }
     
@@ -116,11 +110,19 @@ public class Shift {
     
     @Override
     public String toString() {
+        
+        // "Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)"
 
         StringBuilder s = new StringBuilder();
 
-        s.append('#').append(lunchDuration).append(' ');
-        s.append('(').append(shiftDuration).append(')');
+        s.append(description).append(':');
+        s.append(' ').append(shiftstart).append(' ').append('-');
+        s.append(' ').append(shiftstop).append(' ');
+        s.append('(').append(shiftDuration).append(" minutes)");
+        s.append(';').append(' ').append("Lunch").append(':');
+        s.append(' ').append(lunchstart).append(' ').append('-');
+        s.append(' ').append(lunchstop).append(' ');
+        s.append('(').append(lunchDuration).append(" minutes)");
 
         return s.toString();
 
