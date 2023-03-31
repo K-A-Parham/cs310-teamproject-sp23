49ad29537408e7c53a96408bd85e0c6e4c95b77d
Quasean Coleman <fcoleman2@stu.jsu.edu>
2023-03-31 12:55:03 -0500

    Resolving merge problems

---
+++ src/edu/jsu/mcis/cs310/coursedb/dao/RegistrationDAO.java
@@ -0,0 +1,171 @@
+package edu.jsu.mcis.cs310.coursedb.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class RegistrationDAO {
+    
+    // INSERT YOUR CODE HERE
+     private static final String QUERY_CREATE = "INSERT INTO registration (studentid, termid, crn) VALUES(?, ?, ?)";
+    private static final String QUERY_DELETE = "DELETE FROM registration WHERE studentid = ? AND termid = ? AND crn = ?";
+    private static final String QUERY_DELETE_ALL = "DELETE FROM registration WHERE studentid = ? AND termid = ?";
+    private static final String QUERY_LIST = "SELECT * FROM registration WHERE studentid = ? AND termid = ? ORDER BY crn";
+    private final DAOFactory daoFactory;
+    
+    RegistrationDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+    
+    public boolean create(int studentid, int termid, int crn) {
+        
+        boolean result = false;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        try {
+            
+            Connection conn = daoFactory.getConnection();
+            
+            if (conn.isValid(0)) {
+                
+                // INSERT YOUR CODE HERE
+                   ps = conn.prepareStatement(QUERY_CREATE);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                ps.setInt(3, crn);
+                  int updateCount = ps.executeUpdate();
+                 if (updateCount > 0) {            
+                    result = true;
+                }
+            }
+            
+        }
+        
+        catch (Exception e) { e.printStackTrace(); }
+        
+        finally {
+            
+            if (rs != null) { try { rs.close(); } catch (Exception e) { e.printStackTrace(); } }
+            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); } }
+            
+        }
+        
+        return result;
+        
+    }
+
+    public boolean delete(int studentid, int termid, int crn) {
+        
+        boolean result = false;
+        
+        PreparedStatement ps = null;
+        
+        try {
+            
+            Connection conn = daoFactory.getConnection();
+            
+            if (conn.isValid(0)) {
+                
+                // INSERT YOUR CODE HERE
+                 ps = conn.prepareStatement(QUERY_DELETE);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                ps.setInt(3, crn);
+                int updateCount = ps.executeUpdate();
+                if (updateCount > 0) {
+                    result = true;
+                }
+            }
+            
+        }
+        
+        catch (Exception e) { e.printStackTrace(); }
+        
+        finally {
+
+            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); } }
+            
+        }
+        
+        return result;
+        
+    }
+    
+    public boolean delete(int studentid, int termid) {
+        
+        boolean result = false;
+        
+        PreparedStatement ps = null;
+        
+        try {
+            
+            Connection conn = daoFactory.getConnection();
+            
+            if (conn.isValid(0)) {
+                
+                // INSERT YOUR CODE HERE
+                ps = conn.prepareStatement(QUERY_DELETE_ALL);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                  int updateCount = ps.executeUpdate();
+                if (updateCount > 0) {
+                    result = true;
+                }
+            }
+            
+        }
+        
+        catch (Exception e) { e.printStackTrace(); }
+        
+        finally {
+
+            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); } }
+            
+        }
+        
+        return result;
+        
+    }
+
+    public String list(int studentid, int termid) {
+        
+        String result = null;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ResultSetMetaData rsmd = null;
+        
+        try {
+            
+            Connection conn = daoFactory.getConnection();
+            
+            if (conn.isValid(0)) {
+                
+                // INSERT YOUR CODE HERE
+                 ps = conn.prepareStatement(QUERY_LIST);
+                  ps.setInt(1, studentid);
+                  ps.setInt(2, termid);
+                rs = ps.executeQuery();
+                 result = DAOUtility.getResultSetAsJson(rs);
+            }
+            
+        }
+        
+        catch (Exception e) { e.printStackTrace(); }
+        
+        finally {
+            
+            if (rs != null) { try { rs.close(); } catch (Exception e) { e.printStackTrace(); } }
+            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); } }
+            
+        }
+        
+        return result;
+        
+    }
+    
+}
---
+++ src/edu/jsu/mcis/cs310/coursedb/dao/SectionDAO.java
@@ -0,0 +1,60 @@
+package edu.jsu.mcis.cs310.coursedb.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import com.github.cliftonlabs.json_simple.*;
+public class SectionDAO {
+    
+    // INSERT YOUR CODE HERE
+     String query = "SELECT * FROM section WHERE termid = ? AND subjectid = ? AND num = ? ORDER BY crn";
+    private final DAOFactory daoFactory;
+    
+    SectionDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+    
+    public String find(int termid, String subjectid, String num) {
+        
+        String result = null;
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ResultSetMetaData rsmd = null;
+        
+        try {
+            
+            Connection conn = daoFactory.getConnection();
+            
+            if (conn.isValid(0)) {
+                
+                // INSERT YOUR CODE HERE
+                 ps = conn.prepareStatement(query);
+                ps.setInt(1, termid);
+                ps.setString(2, subjectid);
+                ps.setString(3, num);
+                 boolean hasresults = ps.execute();
+                if (hasresults) {
+                       rs = ps.getResultSet();
+                    result = DAOUtility.getResultSetAsJson(rs);
+                    
+                }
+            }
+            
+        }
+        
+        catch (Exception e) { e.printStackTrace(); }
+        
+        finally {
+            
+            if (rs != null) { try { rs.close(); } catch (Exception e) { e.printStackTrace(); } }
+            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); } }
+            
+        }
+        
+        return result;
+        
+    }
+    
+}
